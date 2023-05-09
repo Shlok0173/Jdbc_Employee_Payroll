5273c8546d99b437e9a63e2cde4c6586994fbff7
Shlok Kumar Mishra <shlok7869@gmail.com>
2023-05-09 17:17:38 +0530

    Added:Update Salary and Name

--- src/com/bridgelabz/student/EmployeeDao.java
+++ src/com/bridgelabz/student/EmployeeDao.java
@@ -1,57 +1,72 @@
 package com.bridgelabz.student;
+
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 
 public class EmployeeDao {
 
-	public static boolean insertEmployeeToDb( EmployeeInformation st) {
-		 boolean f=false;
+	public static boolean insertEmployeeToDb(EmployeeInformation st) {
+		boolean f = false;
 		try {
-			
-			Connection con=CP.create();
-			String q="Insert into Employee_payroll(name,salary,start_date,gender) values(?,?,?,?)";
-			
-			//PreparedStatement
-			
-			  PreparedStatement psmt=con.prepareStatement(q);
-			  psmt.setString(1,st.getName());
-			  psmt.setDouble(2, st.getSalary());
-			  psmt.setString(3, st.getStart_date());
-			  psmt.setString(4, st.getGender());
-			
-			  // Excuted
-			  
-			  psmt.executeUpdate();
-			  f=true;
-		}catch(Exception e) {
+
+			Connection con = CP.create();
+			String q = "Insert into Employee_payroll(name,salary,start_date,gender) values(?,?,?,?)";
+
+			// PreparedStatement
+
+			PreparedStatement psmt = con.prepareStatement(q);
+			psmt.setString(1, st.getName());
+			psmt.setDouble(2, st.getSalary());
+			psmt.setString(3, st.getStart_date());
+			psmt.setString(4, st.getGender());
+
+			// Excuted
+
+			psmt.executeUpdate();
+			f = true;
+		} catch (Exception e) {
 			e.getMessage();
 		}
-		
+
+		return f;
+	}
+
+	public static boolean updateEmployeeToDb(EmployeeInformation st) {
+		boolean f = false;
+		try {
+
+			Connection con = CP.create();
+			String q = "update employee_payroll set salary =30000 where name =?";
+
+			// PreparedStatement
+
+			PreparedStatement psmt = con.prepareStatement(q);
+			psmt.setString(1, st.getName());
+
+			// Excuted
+
+			psmt.executeUpdate();
+			f = true;
+		} catch (Exception e) {
+			e.getMessage();
+		}
+
 		return f;
 	}
 	
-	public static boolean updateEmployeeToDb(EmployeeInformation st) {
-		 boolean f=false;
-			try {
-				
-				Connection con=CP.create();
-				String q="update employee_payroll set salary =30000 where name =?";
-				
-				//PreparedStatement
-				
-				  PreparedStatement psmt=con.prepareStatement(q);
-				  psmt.setString(1,st.getName());
-				
-				  // Excuted
-				  
-				  psmt.executeUpdate();
-				  f=true;
-			}catch(Exception e) {
-				e.getMessage();
-			}
+	public static boolean deleteEmployeeToDp(EmployeeInformation st) {
+		boolean f=false;
+		try {
+			Connection con= CP.create();
+			String q="Delete employee_payroll where name=?";
 			
-			return f;
+			PreparedStatement pmst=con.prepareStatement(q);
+			pmst.setString(1, st.getName());
+			f=true;
+		}catch(Exception e) {
+			e.printStackTrace();
 		}
+		return f;
 	}
-
+}
